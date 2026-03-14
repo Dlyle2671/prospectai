fbdd41a11385c641782e784c580b5bd37b3eb808
Dlyle2671 <dlyle@rrilsolutions.com>
2026-03-14 15:27:29 -0700

    Create next.config.js

---
+++ prospectai-next/next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+        images: {
+    domains: ['zenprospect-production.s3.amazonaws.com', 'apollo-server.com'],
+          remotePatterns: [
+      { protocol: 'https', hostname: '**.amazonaws.com' },
+{ protocol: 'https', hostname: '**.apollo.io' },
+      ],
+  },
+  };
+
+module.exports = nextConfig;
