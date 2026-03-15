0e25eeecce3710c025fc130ccf6abf9a3edea858
Dlyle2671 <dlyle@rrilsolutions.com>
2026-03-14 20:59:57 -0700

    Fix: restore output: standalone in next.config.js for Amplify SSR

--- prospectai-next/next.config.js
+++ prospectai-next/next.config.js
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'standalone',
     reactStrictMode: true,
         images: {
-    domains: ['zenprospect-production.s3.amazonaws.com', 'apollo-server.com'],
-          remotePatterns: [
-      { protocol: 'https', hostname: '**.amazonaws.com' },
+        domains: ['zenprospect-production.s3.amazonaws.com', 'apollo-server.com'],
+            remotePatterns: [
+        { protocol: 'https', hostname: '**.amazonaws.com' },
 { protocol: 'https', hostname: '**.apollo.io' },
-      ],
-  },
-  };
+        ],
+    },
+    };
 
 module.exports = nextConfig;
