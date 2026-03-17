05ecdb3a35bd5d8e2bbfc8dfac6df1207c078ef9
Dlyle2671 <dlyle@rrilsolutions.com>
2026-03-16 18:52:48 -0700

    settings: expand all scoring dropdowns to 0-100 range

--- prospectai-next/components/Settings.js
+++ prospectai-next/components/Settings.js
@@ -38,8 +38,8 @@ export const DEFAULT_ICP = {
 // All keys that count toward the 100-point budget
 const BUDGET_KEYS = ['companySizeWeight','industryWeight','fundingWeight','verifiedEmailBonus','linkedinBonus','phoneBonus','hiringSurgeBonus','awsBonus'];
 
-const WEIGHT_OPTIONS = [0,5,10,15,20,25,30,35,40,45,50];
-const BONUS_OPTIONS  = [0,1,2,3,4,5,6,7,8,9,10,12,15,20,25,30,35,40,45,50];
+const WEIGHT_OPTIONS = [0,5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100];
+const BONUS_OPTIONS  = [0,1,2,3,4,5,6,7,8,9,10,12,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100];
 const THRESHOLD_OPTIONS = Array.from({length: 19}, (_, i) => (i + 1) * 5);
 
 const dropStyle = {
