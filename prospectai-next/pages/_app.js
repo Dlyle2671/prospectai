bc93d5a99fba2c6fa29809d0bf84eb9172e611a0
Dlyle2671 <dlyle@rrilsolutions.com>
2026-03-25 18:19:49 -0700

    Wrap app with UserSettingsProvider for shared settings context

--- prospectai-next/pages/_app.js
+++ prospectai-next/pages/_app.js
@@ -1,10 +1,13 @@
 import { ClerkProvider } from '@clerk/nextjs';
+import { UserSettingsProvider } from '../lib/UserSettingsContext';
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
   return (
     <ClerkProvider {...pageProps}>
-      <Component {...pageProps} />
+      <UserSettingsProvider>
+        <Component {...pageProps} />
+      </UserSettingsProvider>
     </ClerkProvider>
   );
 }
