47b0abbda1540b516e49e80f2e408fd12be4b71c
Dlyle2671 <dlyle@rrilsolutions.com>
2026-03-26 19:29:38 -0700

    Fix SalesAnalytics: add missing React import

--- prospectai-next/components/SalesAnalytics.js
+++ prospectai-next/components/SalesAnalytics.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const SK = 'pai_quotaTracker';
 const CQ = { PS: 6500000, FO: 32000000, MS: 1500000 };
